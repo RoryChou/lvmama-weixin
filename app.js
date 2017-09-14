cc1cee68564230e51e0ace6e65847b06eacad5bc
rory chou <zhouhaotian@lvmama.com>
2017-09-14 17:31:06 +0800

    Signed-off-by: rory chou <zhouhaotian@lvmama.com>

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 //app.js
 App({
+  //全局数据，中文日期，供转换用
+  chineseDate: {
+    years: ['零', '一', '二', '三', '四', '五', '六', '七', '八', '九'],
+    months: ['一', '二', '三', '四', '五', '六', '七', '八', '九', '十', '十一', '十二']
+  },
   onLaunch: function () {
     //调用API从本地缓存中获取数据
     var logs = wx.getStorageSync('logs') || []
@@ -24,7 +29,19 @@ App({
       })
     }
   },
-  globalData:{
-    userInfo:null
+  globalData: {
+    userInfo: null,
+    windowWidth: 0,
+    windowHeight: 0,
+    doubanBase: "https://api.douban.com",
+    inTheaters: "/v2/movie/in_theaters",
+    comingSoon: "/v2/movie/coming_soon",
+    top250: "/v2/movie/top250",
+    weekly: "/v2/movie/weekly",
+    usBox: "/v2/movie/us_box",
+    newMovies: "/v2/movie/new_movies",
+    subject: "/v2/movie/subject/",
+    celebrity: "/v2/movie/celebrity/",
+    search: "/v2/movie/search?q="
   }
 })
